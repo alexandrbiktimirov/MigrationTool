4daf2a88d0da1eafac4fa70e27a3e3d5103c9138
Alexandr Biktimirov <alex.biktimirov38@gmail.com>
2025-09-16 12:52:12 +0200

    feat: add sql constants and json tags for parsing

---
+++ src/main/java/constant/JsonTags.java
@@ -0,0 +1,22 @@
+package constant;
+
+public class JsonTags {
+    public static final String CHANGESET = "changeSet";
+
+    public static final String CREATE_TABLE = "createTable";
+    public static final String ADD_COLUMN = "addColumn";
+    public static final String ADD_CONSTRAINT = "addConstraint";
+    public static final String ADD_INDEX = "addIndex";
+    public static final String RENAME_TABLE = "renameTable";
+    public static final String RENAME_COLUMN = "renameColumn";
+    public static final String MODIFY_COLUMN_TYPE = "modifyColumnType";
+    public static final String DROP_COLUMN = "dropColumn";
+    public static final String DROP_TABLE = "dropTable";
+    public static final String DROP_CONSTRAINT = "dropConstraint";
+    public static final String DROP_INDEX = "dropIndex";
+    public static final String ROLLBACK = "rollback";
+
+    public static final String COLUMN = "column";
+    public static final String CONSTRAINT = "constraint";
+    public static final String INDEX = "index";
+}
